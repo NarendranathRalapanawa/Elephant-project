43a4e78c1854167a14a7e38547b728c92f89db8e
Narendranath Ralapanawa <73179205+NarendranathRalapanawa@users.noreply.github.com>
2024-11-20 03:04:32 -0800

    Add files via upload

--- an_int.ino
+++ an_int.ino
@@ -1,23 +1,52 @@
 
 #include <TimerOne.h>
 volatile int sesv;
-
-
+int n =0;
+int sesv_arr[50];
+bool printst=false;// to find the state of data transmitte
+bool datast=false;// to find the the state of datacollecting
+int a=0;
 void setup () {
  pinMode (A0, INPUT_PULLUP);
- Timer1.initialize(2000);
+ Timer1.initialize(5000);
  Timer1.attachInterrupt(function);
  Serial.begin(9600);
 }
 
 void function () {
  sesv = analogRead(0);
- if(sesv>30){
- Serial.println(sesv);
+ //Serial.println(n);
+ //Serial.println(datast);
+ if(sesv>100){
+  datast=true;
+ // Serial.println("can be elephant");
+ }
+
+ if(datast && n<50){
+  printst=false;
+  sesv_arr[n]=sesv;
+  n=n+1;
+ }
+
+ else if (n==50){
+  printst=true;
+  datast=false;
+
 }
 }
 
 void loop () {
- Serial.println("I am delaying");
- delay(10000);
+  if(printst){
+      
+  for(int i = 0; i <  sizeof(sesv_arr); i++)
+{
+  Serial.println(sesv_arr[i]);
 }
+Serial.println(n);
+n=0;
+printst=0;
+
+ 
+  }
+}
+
